1054f4668f0f622966e0811aca6ec2e7dac44b56
mojtabababiker <mojmohammad98@gmail.com>
2025-05-22 21:37:25 +0300

    update: update the effected components and pages  to correctly implement the auth flow managed by the middleware
    - add the `signup` functionally on the auth page
    - update the oauth components to correctly save and delete the quiz result on the local storage

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,17 +1,50 @@
 "use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 import LoginForm from "@/components/auth/LoginForm";
 import SignupForm from "@/components/auth/SignupForm";
 import Bounded from "@/components/common/Bounded";
-import { useState } from "react";
+import CustomToast from "@/components/common/CustomToast";
+import { useAuth } from "@/context/AuthProvider";
 
 function Page() {
+  const { user, isLoggedIn } = useAuth();
   const [formType, setFormType] = useState<"login" | "signup">("login");
+  const router = useRouter();
 
   const onSignin = () => {
-    // Handle sign-in logic here
+    if (!user) return;
+    // handle the case where the user is login
     console.log("User signed in");
+    if (isLoggedIn && user.skillLevel) {
+      console.log("User created");
+      router.replace("/dashboard");
+      return;
+    }
+    // redirect the user to the complete signup page to take the quiz if not already done
+    router.replace("/auth/oauth/complete-signup");
   };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const msg = urlParams.get("msg");
+    const provider = urlParams.get("provider");
+    if (msg === "oauth-field") {
+      toast.custom((t) => (
+        <CustomToast
+          t={t}
+          type="error"
+          message={`OAuth provider failed: ${provider}`}
+        />
+      ));
+    } else if (msg) {
+      toast.custom((t) => (
+        <CustomToast t={t} type="error" message={msg.replaceAll("+", " ")} />
+      ));
+    }
+  }, []);
   return (
     <Bounded className="relative brief-container">
       <div className="w-full flex items-center justify-center min-h-screen">
