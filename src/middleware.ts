f2a29bdc17f4d9623d9b66aa71d983c3b24c594e
mojtabababiker <mojmohammad98@gmail.com>
2025-05-22 21:33:50 +0300

    feat: add middleware for user authorization and route protecting

---
+++ src/middleware.ts
@@ -0,0 +1,47 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { Models } from "node-appwrite";
+
+import { createUserClient } from "@/config/appwrite";
+
+export async function middleware(request: NextRequest) {
+  console.log("Middleware running");
+  const cookieStore = await cookies();
+  const session = cookieStore.get("appwrite-session");
+  if (!session || !session.value) {
+    console.log("No session found");
+    return NextResponse.redirect(
+      new URL("/auth?msg=need+to+login+to+access", request.url)
+    );
+  }
+  let userAccount: Models.User<Models.Preferences>;
+  try {
+    const { account } = await createUserClient(session?.value);
+    userAccount = await account.get();
+  } catch (error) {
+    return NextResponse.redirect(
+      new URL("/auth?msg=need+to+login+to+access", request.url)
+    );
+  }
+
+  if (!userAccount) {
+    return NextResponse.redirect(
+      new URL("/auth?msg=need+to+login+to+access", request.url)
+    );
+  }
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    "/dashboard",
+    "/playground",
+    "/api/get-problems",
+    "/api/generate_program",
+    "/api/get-code-review",
+    "/api/get-problem-hint",
+    "/api/submit-code",
+  ],
+};
